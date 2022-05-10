28752e019c5c0040628ac831677fa7b6a5a74b3a
aleksimelanen94 <1802185@laurea.fi>
2022-05-10 21:37:44 +0300

    Final not working project

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,54 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
+import Button from '@mui/material/Button';
 import './styles.css'
 const App = ()=> {
-    const[User, setUsers] = useState([])
-    const[variable,setVariable] = useState([])
 
     const UserPrint = (props)=>{
+        const data = {props}
         return (
             <>
-                <h1>{props.username}</h1>
-                <p>{props.password}</p>
+                <h1>{data.username}</h1>
+                <p>{data.password}</p>
+                <p>{data.birthday}</p>
             </>
         )
     }
 
-    const handleChange = (props) =>{
-        console.log(props.target.value);
-        setVariable(props.target.value);
-    } 
+    const [query, setQuery] = useState('')
+    // Define first button handler
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("This happend: ", event.target);
+        console.log("id: ", query);
+        getData();
+    };
 
-    useEffect(() => {
-        let UserCopy = [...User]
-        UserCopy = UserCopy.filter(data => data.username.includes(variable))
-        setUsers(UserCopy)
-    }, [variable])
+    const [results, setResults] = useState([])
 
     const getData = () => {
         fetch("http://localhost:8080/api/getall")
-        .then((result) => {
-            console.log(result)
-            return result.json();
-    })
-        .then((data) => {
-            console.log(data)
-            setUsers(data)
-    });
-} 
+        .then((results) => {
+            return results.json();
+        })
+            .then((data) => {
+                console.log(data);
+                const items = data;
+                setResults(items)
+            });
+};
     return (
         <> 
             <div>
                 <h1>User List</h1>
-                <form>
-                    <label>Search: </label> 
-                    <input value={variable} type="text" onChange={handleChange}/>
-                </form>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Search by id: </label>
+                        <input type="search" value={query} onChange={(event) => setQuery(event.target.value)} placeholder="Set id " name="query"/>
+                    </div>
+                    <div>
+                        <label>Search by username: </label>
+                        <input type="search" value={query} onChange={(event) => setQuery(event.target.value)} placeholder="Username " name="query"/>
+                    </div>
+                    <div>
+                        <Button variant="contained" type="submit">Submit</Button>
+                    </div>
+            </form>
+                <div>
                 {
-                    User?
-                    User.map((data,index) => <UserPrint key={index} username={data.username}   birthday={data.password} />) 
-                    : <div>No Users found. Fething data...</div>
+                    <UserPrint data ={results}/>
                 }
-                <button onClick={getData}>Get Data</button>
+                </div>
+                <Button variant="contained" onClick={getData}>Get all Data</Button>
             </div>
         </>
     )
